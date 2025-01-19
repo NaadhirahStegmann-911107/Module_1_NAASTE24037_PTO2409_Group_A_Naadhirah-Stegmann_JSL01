78a0486f5f550911697f254df06b6d96278e870d
NaadhirahStegmann-911107 <naadhirahsteg@gmail.com>
2025-01-19 23:27:15 +0200

    Validation Scenario
    Created and added the validation scenario to script.js in order to create the correct parameters for the desired results.

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
+    // Validation logic goes here 
+    console.log('Input received:', input); // Logged the inpit for debugging
+    //Initialise the result variable
     let result = ''; // Placeholder for validation result
+     if (input.indexOf('pet_') !==0) {// Check if the input starts with 'pet_'
+        result = 'Invalid input: Must start with "pet_"';
+        console.log(result);
+    } else {
+        let remainingString = input.substring(4);
+        console.log('Remaining string after removing "pet_":', remainingString);
+        let pattern = /^\d{4}[A-Za-z]+$/;
+        if (pattern.test(remainingString)) {
+            result = 'Vaild input';
+            console.log(result);
+        } else {
+            result= 'Invalid input: Must contain 4 digits followed by letters with no spaces';
+            console.log(result);
+            }
+    }    
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
             document.getElementById('result').innerText = result;
 }
-
 
